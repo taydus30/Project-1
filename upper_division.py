a75ce730bc23e9bf68bdc1f7b8579c9fac0fe3f1
Dustyn Taylor <taydus30@evergreen.edu>
2019-05-02 23:12:25 -0700

    decriptions

--- upper_division.py
+++ upper_division.py
@@ -5,12 +5,22 @@ import json
 class Dataset():
 
     def __init__(self, nx):
+        """ The Dataset is a collection of calculated data and visualization
+        -----
+        nx : int array
+            uses an array of population over data
+        """
         self.Nx = nx
         self.Lx = None
         self.Dx = None
         self.Qx = None
 
     def xaxis (self, data):
+        """ Calculates an array for the xaxis of the graphs
+        -----
+        data : int array
+            uses and array of data
+        """
         x = []
 
         for i in range(len(data)):
@@ -19,6 +29,17 @@ class Dataset():
         return x
 
     def output(self, data, title, xlable, ylable):
+        """ Creates and shows a graph of given data
+        -----
+        data : int array
+            uses an array of data
+        title : String
+            Used as the title of the graph
+        xlable : String
+            Used as the lable for the xaxis
+        ylable : String
+            used as the lable for the y axis
+        """
         #show pyplot
         x = self.xaxis(data)
         plt.plot(x, data)
@@ -28,24 +49,32 @@ class Dataset():
         plt.show()
 
     def calcLx(self):
+        """ Calculates the Lx values and creates an array
+        """
         self.Lx = []
 
         for i in range (len(self.Nx)):
             self.Lx.append(self.Nx[i]/self.Nx[0])
 
     def calcDx(self):
+        """ Calculates the Dx values and creates an array
+        """
         self.Dx = []
 
         for i in range(len(self.Lx)-1):
             self.Dx.append(self.Lx[i]-self.Lx[i+1])
 
     def calcQx(self):
+        """ Calculates the Qx values and creates an array
+        """
         self.Qx = []
 
         for i in range (len(self.Dx)):
             self.Qx.append(self.Dx[i]/self.Lx[i])
 
     def printTable(self):
+        """ Formulates and prints the data in a nice table
+        """
         Table = []
 
         for i in range(len(Nx)):
@@ -59,6 +88,8 @@ class Dataset():
         print(tabulate(Table, headers = ['Nx', 'lx', 'dx', 'qx']))
 
     def graphAll(self):
+        """ Graphs all of the Nx, Lx, Dx, Qx data
+        """
         self.xaxis(self.Nx)
         self.output(self.Nx, 'Population Over Time', 'Population', 'Years')
 
