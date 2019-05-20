cd1d06c7c22240b3c4e5e3f23739df65482a911f
Dustyn Taylor <taydus30@evergreen.edu>
2019-05-19 23:07:04 -0700

    added base_frequencies

--- project 2/project_2.py
+++ project 2/project_2.py
@@ -35,15 +35,37 @@ class DNA:
     def mutation(self):
         bases = 'ATCG'
         r = random.choice(bases)
-        index = random.randint(0, len(self))
-        out = list(self)
-        while out == list(self):
+        index = random.randint(0, len(self.strand))
+        out = list(self.strand)
+        while out == list(self.strand):
             out[index] = r
         return(''.join(out))
 
+    def base_frequencies(self):
+        count_a = 0
+        count_t = 0
+        count_c = 0
+        count_g = 0
+
+        for i in self.strand:
+            if i == 'a' or i == 'A':
+                count_a += 1
+            if i == 't' or i == 'T':
+                count_t += 1
+            if i == 'c' or i == 'C':
+                count_c += 1
+            if i == 'g' or i == 'G':
+                count_g += 1
+
+        percent_a = round((count_a / len(self.strand))*100,3)
+        percent_t = round((count_t / len(self.strand))*100,3)
+        percent_c = round((count_c / len(self.strand))*100,3)
+        percent_g = round((count_g / len(self.strand))*100,3)
+        return(percent_a, percent_t, percent_c, percent_g)
 
 
 nnn = DNA("atcg")
 print(nnn.compliment())
 print(nnn.mrna())
 print(nnn.mutation())
+print(nnn.base_frequencies())
